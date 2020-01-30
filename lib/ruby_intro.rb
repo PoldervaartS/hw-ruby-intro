63b9d69bea35befd8c2bd02a10fc9509e9581f47
EC2 Default User <ec2-user@ip-172-31-46-198.us-east-2.compute.internal>
2020-01-30 21:01:51 +0000

    Homework Finished Spec log generated

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,82 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sumarr = 0
+  arr.each do |num|
+    sumarr = sumarr + num
+  end
+  return sumarr
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return arr.max(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  for i in 0...(arr.length-1)
+    for j in (i+1)...arr.length
+      if arr[i] + arr[j] == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return ("Hello, #{name}")
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  consonants = ['a','e','i','o','u']
+  if ( (s.length == 0) || !(s[0] =~ /[[:alpha:]]/) )
+    return false
+  end
+  
+  for letter in consonants do
+    if(s.downcase[0] == letter)
+      return false
+    end
+  end
+  return true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  chars = s.split('')
+  for letter in chars
+    if (letter != '0' && letter != '1')
+      return false
+    end
+  end
+  if(s.length == 0)
+    return false
+  end
+  begin
+    return (s.to_i(2) % 4) == 0
+  rescue 
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  @isbn
+  @price
+  def initialize(isbn, price)
+    if(isbn == '' || price <= 0)
+      raise ArgumentError, 'One of the values is not valid'
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def price_as_string
+    return "\$%0.2f" % [price]
+  end
 end
